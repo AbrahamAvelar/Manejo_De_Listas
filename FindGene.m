d84a8ae34cd9fbb3000808f0b284af721a0b0b80
AbrahamAvelar <jabrahamavelar@gmail.com>
2019-09-20 12:13:01 -0500

    udate, find genes multiple times index

--- FindGene.m
+++ FindGene.m
@@ -1,14 +1,23 @@
-function i = FindGene( GeneName, ListOfGenes )
-% i = FindGene( GeneName, ListOfGenes )
+function resultado = FindGene( GeneName, ListOfGenes, multiple )
+% i = FindGene( GeneName, ListOfGenes, multiple )
+if nargin<3
+    multiple=0;
+end
     bandera=1;
+    resultado=[];
     for i=1:length(ListOfGenes)
         if strcmp(GeneName, ListOfGenes(i) );
             bandera=0;
-            break
+            if multiple
+                resultado=[resultado i];
+            else
+                resultado=i;
+                break
+            end
         end
     end
     if bandera
-        %warning(strcat('No se encontró el gen: ', GeneName) )
-        i=NaN;
+        %warning(strcat('No se encontrÃ³ el gen: ', GeneName) )
+        resultado=NaN;
     end
 end
